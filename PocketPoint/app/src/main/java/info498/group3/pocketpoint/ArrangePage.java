d7b56496d2075bea230ff87893156d235d2da692
Jacob Gilmore <jsg3733@uw.edu>
2015-05-27 15:27:31 -0700

    Makes it so only the icons that can be removed have there X's show when on the arrange page. 	Added global variables to make it easier to access information on the arrange page and removed the full initialization of these later on so just use the variable name.  Also, moved the location of the backButton so was below initialization of the iconBar.  Also, receive the category from the previous Intent so that can pass back. Made it so the back button passes back the information need to the WordPage so that it can run with the bar loaded if need be (Similar to the one passing back to Category Page from Word Page). 	Made it so that the remove X's can be clicked on to remove the icon from the list.  Will remove the given information and shift the stuff from the right over to the left.  Also, it makes the current information invisible so that will not show (used similar code to gridBarWithFragment).  Lastly, made a makeInvisible method that is used by the removeIcon onClickListener to set the visibility of the title, image, and remove sign to invisible. 	Made it so GridFragmentWithBar sends the topic/category to the arrange Page when the ArrangePage button is pressed.

--- PocketPoint/app/src/main/java/info498/group3/pocketpoint/ArrangePage.java
+++ PocketPoint/app/src/main/java/info498/group3/pocketpoint/ArrangePage.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -29,6 +30,22 @@ public class ArrangePage extends ActionBarActivity {
 
     private List<Icon> iconBar;
     private int howManyInBar;
+    private TextView titleOne;
+    private TextView titleTwo;
+    private TextView titleThree;
+    private TextView titleFour;
+    private ImageView imgOne;
+    private ImageView imgTwo;
+    private ImageView imgThree;
+    private ImageView imgFour;
+
+    private ImageView removeIconOne;
+    private ImageView removeIconTwo;
+    private ImageView removeIconThree;
+    private ImageView removeIconFour;
+
+    private String topic;
+
 
 
     @Override
@@ -40,12 +57,6 @@ public class ArrangePage extends ActionBarActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        LinearLayout backButton = (LinearLayout) findViewById(R.id.backButton);
-        backButton.setOnClickListener(new LinearLayout.OnClickListener() {
-            public void onClick(View v) {
-                finish();
-            }
-        });
 
         // With serializable and passing extra (problem with Bitmap images)
         //Intent launchedMe = getIntent();
@@ -60,6 +71,7 @@ public class ArrangePage extends ActionBarActivity {
 
         iconBar = new ArrayList<Icon>();
         Intent launchedMe = getIntent();
+        topic = launchedMe.getStringExtra("category");
         howManyInBar = launchedMe.getIntExtra("howManyInBar", 1);
         List<String> iconNumber = new ArrayList<String>(
                 Arrays.asList("iconOne", "iconTwo", "iconThree", "iconFour"));
@@ -88,15 +100,56 @@ public class ArrangePage extends ActionBarActivity {
             }
         }
 
+        LinearLayout backButton = (LinearLayout) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new LinearLayout.OnClickListener() {
+            public void onClick(View v) {
+                if(iconBar.size() > 0) {
+                    Intent categoryPage = new Intent(ArrangePage.this, WordPage.class);
+                    List<String> iconNumber = new ArrayList<String>(
+                            Arrays.asList("iconOne", "iconTwo", "iconThree", "iconFour"));
+                    for(int i = 0; i < howManyInBar; i++) {
+                        Icon current = iconBar.get(i);
+                        String iconNum = iconNumber.get(i);
 
+                        categoryPage.putExtra(iconNum + "Title",  current.getTitle());
+                        if(current.getIcon() < 0 ) {
+                            categoryPage.putExtra(iconNum + "ImageInt", 1);
+                            //arrange.putExtra(iconNum + "Img", current.getBitmap());
+                        }else {
+                            categoryPage.putExtra(iconNum + "ImageInt", 0);
+                            categoryPage.putExtra(iconNum + "Img", current.getIcon());
+                        }
+                    }
+                    categoryPage.putExtra("howManyInBar", howManyInBar);
+                    categoryPage.putExtra("category", topic);
+                    if(howManyInBar > 0){
+                        categoryPage.putExtra("showBar", true);
+                    }
+                    //categoryPage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(categoryPage);
+                }
+                finish();
 
+            }
+        });
+
+        // finding the imageviews for the X's in the corners of the icons within the iconBar
+        removeIconOne = (ImageView) findViewById(R.id.btnRemoveOne);
+        removeIconTwo = (ImageView) findViewById(R.id.btnRemoveTwo);
+        removeIconThree = (ImageView) findViewById(R.id.btnRemoveThree);
+        removeIconFour = (ImageView) findViewById(R.id.btnRemoveFour);
+        // setting the onClick listener for all of the X's
+        removeIconOne.setOnClickListener(removeIcon);
+        removeIconTwo.setOnClickListener(removeIcon);
+        removeIconThree.setOnClickListener(removeIcon);
+        removeIconFour.setOnClickListener(removeIcon);
 
 
         Log.i("iconOne", iconBar.get(0).getTitle());
         if(iconBar.size() >= 1) {
-            TextView titleOne = (TextView) findViewById(R.id.txtTitleOne);
+            titleOne = (TextView) findViewById(R.id.txtTitleOne);
             titleOne.setText(iconBar.get(0).getTitle());
-            ImageView imgOne = (ImageView) findViewById(R.id.imgIconOne);
+            imgOne = (ImageView) findViewById(R.id.imgIconOne);
             if (iconBar.get(0).getIcon() < 0) {
                 imgOne.setImageBitmap(iconBar.get(0).getBitmap());
             } else {
@@ -104,34 +157,37 @@ public class ArrangePage extends ActionBarActivity {
             }
         }
         if(iconBar.size() >= 2) {
-            TextView titleTwo = (TextView) findViewById(R.id.txtTitleTwo);
+            titleTwo = (TextView) findViewById(R.id.txtTitleTwo);
             titleTwo.setText(iconBar.get(1).getTitle());
-            ImageView imgTwo = (ImageView) findViewById(R.id.imgIconTwo);
+            imgTwo = (ImageView) findViewById(R.id.imgIconTwo);
             if(iconBar.get(1).getIcon() < 0 ){
                 imgTwo.setImageBitmap(iconBar.get(1).getBitmap());
             }else {
                 imgTwo.setImageResource(iconBar.get(1).getIcon());
             }
+            removeIconTwo.setVisibility(View.VISIBLE);
         }
         if(iconBar.size() >= 3) {
-            TextView titleThree = (TextView) findViewById(R.id.txtTitleThree);
+            titleThree = (TextView) findViewById(R.id.txtTitleThree);
             titleThree.setText(iconBar.get(2).getTitle());
-            ImageView imgThree = (ImageView) findViewById(R.id.imgIconThree);
+            imgThree = (ImageView) findViewById(R.id.imgIconThree);
             if(iconBar.get(2).getIcon() < 0 ){
                 imgThree.setImageBitmap(iconBar.get(2).getBitmap());
             }else {
                 imgThree.setImageResource(iconBar.get(2).getIcon());
             }
+            removeIconThree.setVisibility(View.VISIBLE);
         }
         if(iconBar.size() >= 4) {
-            TextView titleFour = (TextView) findViewById(R.id.txtTitleFour);
+            titleFour = (TextView) findViewById(R.id.txtTitleFour);
             titleFour.setText(iconBar.get(3).getTitle());
-            ImageView imgFour = (ImageView) findViewById(R.id.imgIconFour);
+            imgFour = (ImageView) findViewById(R.id.imgIconFour);
             if(iconBar.get(3).getIcon() < 0 ){
                 imgFour.setImageBitmap(iconBar.get(3).getBitmap());
             }else {
                 imgFour.setImageResource(iconBar.get(3).getIcon());
             }
+            removeIconFour.setVisibility(View.VISIBLE);
         }
 
         Button done = (Button) findViewById(R.id.btnDone);
@@ -160,6 +216,167 @@ public class ArrangePage extends ActionBarActivity {
             }
         });
 
+    }
+
+    // Is called when an X is clicked
+    // Will remove that icon from the bar and shift the ones from
+    // the right over to the left
+    private View.OnClickListener removeIcon = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.btnRemoveOne:
+                    switch (howManyInBar){
+                        case 1:
+                            //iconOne.setVisibility(View.INVISIBLE);
+                            /*titleOne.setVisibility(View.INVISIBLE);
+                            imgOne.setVisibility(View.INVISIBLE);
+                            removeIconOne.setVisibility(View.INVISIBLE);*/
+                            makeInvisible(1);
+                            break;
+                        case 2:
+                            titleOne.setText(titleTwo.getText());
+                            if(iconBar.get(1).getIcon() < 0){
+                                imgOne.setImageBitmap(iconBar.get(1).getBitmap());
+                            }else {
+                                imgOne.setImageResource(iconBar.get(1).getIcon());
+                            }
+                            //iconTwo.setVisibility(View.INVISIBLE);
+                            /*titleTwo.setVisibility(View.INVISIBLE);
+                            imgTwo.setVisibility(View.INVISIBLE);
+                            removeIconTwo.setVisibility(View.INVISIBLE);*/
+                            makeInvisible(2);
+                            break;
+                        case 3:
+                            titleOne.setText(titleTwo.getText());
+                            if(iconBar.get(1).getIcon() < 0){
+                                imgOne.setImageBitmap(iconBar.get(1).getBitmap());
+                            }else {
+                                imgOne.setImageResource(iconBar.get(1).getIcon());
+                            }
+                            titleTwo.setText(titleThree.getText());
+                            if(iconBar.get(2).getIcon() < 0){
+                                imgTwo.setImageBitmap(iconBar.get(2).getBitmap());
+                            }else {
+                                imgTwo.setImageResource(iconBar.get(2).getIcon());
+                            }
+                           //iconThree.setVisibility(View.INVISIBLE);
+                            makeInvisible(3);
+                            break;
+                        case 4:
+                            titleOne.setText(titleTwo.getText());
+                            if(iconBar.get(1).getIcon() < 0){
+                                imgOne.setImageBitmap(iconBar.get(1).getBitmap());
+                            }else {
+                                imgOne.setImageResource(iconBar.get(1).getIcon());
+                            }
+                            titleTwo.setText(titleThree.getText());
+                            if(iconBar.get(2).getIcon() < 0){
+                                imgTwo.setImageBitmap(iconBar.get(2).getBitmap());
+                            }else {
+                                imgTwo.setImageResource(iconBar.get(2).getIcon());
+                            }
+                            titleThree.setText(titleFour.getText());
+                            if(iconBar.get(3).getIcon() < 0){
+                                imgThree.setImageBitmap(iconBar.get(3).getBitmap());
+                            }else {
+                                imgThree.setImageResource(iconBar.get(3).getIcon());
+                            }
+                            //iconFour.setVisibility(View.INVISIBLE);
+                            makeInvisible(4);
+                            break;
+                    }
+                    howManyInBar--;
+                    iconBar.remove(0);
+                    Log.i("Remove", "Icon One");
+                    break;
+                case R.id.btnRemoveTwo:
+                    switch (howManyInBar) {
+                        case 2:
+                           // iconTwo.setVisibility(View.INVISIBLE);
+                            makeInvisible(2);
+                            break;
+                        case 3:
+                            titleTwo.setText(titleThree.getText());
+                            if(iconBar.get(2).getIcon() < 0){
+                                imgTwo.setImageBitmap(iconBar.get(2).getBitmap());
+                            }else {
+                                imgTwo.setImageResource(iconBar.get(2).getIcon());
+                            }
+                            //iconThree.setVisibility(View.INVISIBLE);
+                            makeInvisible(3);
+                            break;
+                        case 4:
+                            titleTwo.setText(titleThree.getText());
+                            if(iconBar.get(2).getIcon() < 0){
+                                imgTwo.setImageBitmap(iconBar.get(2).getBitmap());
+                            }else {
+                                imgTwo.setImageResource(iconBar.get(2).getIcon());
+                            }
+                            titleThree.setText(titleFour.getText());
+                            if(iconBar.get(3).getIcon() < 0){
+                                imgThree.setImageBitmap(iconBar.get(3).getBitmap());
+                            }else {
+                                imgThree.setImageResource(iconBar.get(3).getIcon());
+                            }
+                            //iconFour.setVisibility(View.INVISIBLE);
+                            makeInvisible(4);
+                            break;
+                    }
+                    howManyInBar--;
+                    iconBar.remove(1);
+                    Log.i("Remove", "Icon Two");
+                    break;
+                case R.id.btnRemoveThree:
+                    switch (howManyInBar) {
+                        case 3:
+                            //iconThree.setVisibility(View.INVISIBLE);
+                            makeInvisible(3);
+                            break;
+                        case 4:
+                            titleThree.setText(titleFour.getText());
+                            if(iconBar.get(3).getIcon() < 0){
+                                imgThree.setImageBitmap(iconBar.get(3).getBitmap());
+                            }else {
+                                imgThree.setImageResource(iconBar.get(3).getIcon());
+                            }
+                            //iconFour.setVisibility(View.INVISIBLE);
+                            makeInvisible(4);
+                            break;
+                    }
+                    howManyInBar--;
+                    iconBar.remove(2);
+                    Log.i("Remove", "Icon Three");
+                    break;
+                case R.id.btnRemoveFour:
+                    howManyInBar--;
+                    iconBar.remove(3);
+                    //iconFour.setVisibility(View.INVISIBLE);
+                    makeInvisible(4);
+                    Log.i("Remove", "Icon Four");
+                    break;
+            }
+        }
+    };
+
+    private void makeInvisible(int num){
+        if(num == 1){
+            titleOne.setVisibility(View.INVISIBLE);
+            imgOne.setVisibility(View.INVISIBLE);
+            removeIconOne.setVisibility(View.INVISIBLE);
+        }else if(num == 2) {
+            titleTwo.setVisibility(View.INVISIBLE);
+            imgTwo.setVisibility(View.INVISIBLE);
+            removeIconTwo.setVisibility(View.INVISIBLE);
+        }else if(num == 3) {
+            titleThree.setVisibility(View.INVISIBLE);
+            imgThree.setVisibility(View.INVISIBLE);
+            removeIconThree.setVisibility(View.INVISIBLE);
+        }else {
+            titleFour.setVisibility(View.INVISIBLE);
+            imgFour.setVisibility(View.INVISIBLE);
+            removeIconFour.setVisibility(View.INVISIBLE);
+        }
     }
 
 
--- PocketPoint/app/src/main/java/info498/group3/pocketpoint/GridFragmentWithBar.java
+++ PocketPoint/app/src/main/java/info498/group3/pocketpoint/GridFragmentWithBar.java
@@ -432,6 +432,7 @@ public class GridFragmentWithBar extends Fragment {
                     }
                 }
                 arrange.putExtra("howManyInBar", howManyInBar);
+                arrange.putExtra("category", topic);
                 startActivity(arrange);
             }
         });
